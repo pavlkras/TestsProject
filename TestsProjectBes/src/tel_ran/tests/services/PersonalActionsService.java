757325de1ad5a45481b59e2a688178459d0d507a
Yuri Granovsky <yuri@inetex.co.il>
2015-02-05 23:16:44 +0200

    test

--- TestsProjectBes/src/tel_ran/tests/services/PersonalActionsService.java
+++ TestsProjectBes/src/tel_ran/tests/services/PersonalActionsService.java
@@ -3,7 +3,7 @@ package tel_ran.tests.services;
 import tel_ran.tests.services.interfaces.IPersonalActionsService;
 
 public class PersonalActionsService extends TestsPersistence implements IPersonalActionsService {
-	/*............................
+	/*oooooooo
 	 *
 	 * 
 	 * 
