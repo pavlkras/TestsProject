2c91ec915a38a05c31aec0fa377fe0d6d1c3cf84
fen1979 <00000001@nm.ru>
2015-02-06 22:01:10 +0200

    Revert "refresh code"
    This reverts commit dc9326a4f25ba27bf45e1266efca7c325971aa20.

--- TestsProjectCommon/src/tel_ran/tests/services/interfaces/IMaintenanceService.java
+++ TestsProjectCommon/src/tel_ran/tests/services/interfaces/IMaintenanceService.java
@@ -1,16 +1,5 @@
 package tel_ran.tests.services.interfaces;
 
-import java.util.List;
-
-public interface IMaintenanceService {//общий интерфейс 
-	//Сюда добавляем свои методы для работы с ними через веб. 
-	//ВАЖНО ЭТО ИНТЕРФЕЙС !!!-ТЕСТ-!!! при изменениях ихменить инрерфейс !!!-СЕРВИС-!!!.
-
-	boolean createQuestion(String questionText,String descriptionText,String category,int level,List<String> answers,int trueAnswerNumber);
-	public String getInformation(String questionKey);
-	boolean AddQuestionsFromFile(String FileName);
-	String SearchQuestionInDataBase(String question, String category);
-	String UpdateQuestionInDataBase(String questionID, String questionText,
-			String descriptionText, String category, int question_level,
-			List<String> answer, int trueAnswerNumber);
+public interface IMaintenanceService {
+void addQuestion();
 }
