b60b482f185116b831ff13985df51128e48fcb70
AlexFoox <alexfoox@gmail.com>
2015-02-06 19:03:58 +0200

    add my interafceMETODs

--- TestsProjectCommon/src/tel_ran/tests/services/interfaces/ICompanyActionsService.java
+++ TestsProjectCommon/src/tel_ran/tests/services/interfaces/ICompanyActionsService.java
@@ -1,4 +1,8 @@
 package tel_ran.tests.services.interfaces;
 public interface ICompanyActionsService {
 void printQuestion(int id);
+
+//alexfoox Company 
+boolean createCompany(String C_Name,String C_Site, String C_Specialization,String C_AmountEmployes,String C_Password);
+String []getAnySingleQuery(String strQuery);
 }
