eb55e035e43e1535ecddce6d997e30608abc4e9b
fen1979 <00000001@nm.ru>
2015-02-07 00:54:59 +0200

    changed interface maintenance

--- TestsProjectCommon/src/tel_ran/tests/services/interfaces/IMaintenanceService.java
+++ TestsProjectCommon/src/tel_ran/tests/services/interfaces/IMaintenanceService.java
@@ -1,5 +1,16 @@
 package tel_ran.tests.services.interfaces;
 
-public interface IMaintenanceService {
-void addQuestion();
+import java.util.List;
+
+public interface IMaintenanceService {//общий интерфейс 
+	//Сюда добавляем свои методы для работы с ними через веб. 
+	boolean createQuestion(String questionText,String descriptionText,String category,int level,List<String> answers,int trueAnswerNumber);
+	public String getInformation(String questionKey);
+	boolean FillDataBaseFromTextResource(List<String> inputParsedText);
+	String SearchQuestionInDataBase(String question, String category);
+	String UpdateQuestionInDataBase(String questionID, String questionText,
+			String descriptionText, String category, int question_level,
+			List<String> answer, int trueAnswerNumber);
+	// method for group generate test
+	List<String> GeneratedTestQuestion(String category,String level);
 }
