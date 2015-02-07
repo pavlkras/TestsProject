153fa54c304eef54a55127f8f69133c534321896
Amir <fen1979@users.noreply.github.com>
2015-02-08 01:53:43 +0200

    changed  return for any methods

--- TestsProjectCommon/src/tel_ran/tests/services/interfaces/IMaintenanceService.java
+++ TestsProjectCommon/src/tel_ran/tests/services/interfaces/IMaintenanceService.java
@@ -5,12 +5,16 @@ import java.util.List;
 public interface IMaintenanceService {//общий интерфейс 
 	//Сюда добавляем свои методы для работы с ними через веб. 
 	boolean createQuestion(String questionText,String descriptionText,String category,int level,List<String> answers,int trueAnswerNumber);
-	public String getInformation(String questionKey);
-	boolean FillDataBaseFromTextResource(List<String> inputParsedText);
-	String SearchQuestionInDataBase(String question, String category);
-	String UpdateQuestionInDataBase(String questionID, String questionText,
+	//Метод обновляет вопрос в БД
+	boolean UpdateQuestionInDataBase(String questionID, String questionText,
 			String descriptionText, String category, int question_level,
 			List<String> answer, int trueAnswerNumber);
-	// method for group generate test
-	List<String> GeneratedTestQuestion(String category,String level);
+	//Поиск по БД возвращает Лист Стрингов : принимает 2 параметра но поиск идет по question
+	public List<String> SearchQuestionInDataBase(String question, String category);
+	//Метод добавления данных в БД из файла на удаленном компьютере
+	boolean FillDataBaseFromTextResource(List<String> inputParsedText);		
+	//Возвращает 1 вопрос с ответами в стринге: поиск по ID Question
+	public String getInformation(String questionKey);
+	// method for group generate test. AlexFoox
+	public List<String> generatedTestQuestion(String category,String level);
 }
