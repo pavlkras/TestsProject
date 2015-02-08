dd95fda25712c5ef84d6e7a3a40b1ab921024fb3
Igor <igor.tymoshchuk@gmail.com>
2015-02-08 05:13:23 +0200

    3.1.4. Added interface methods for company actions

--- TestsProjectCommon/src/tel_ran/tests/services/interfaces/ICompanyActionsService.java
+++ TestsProjectCommon/src/tel_ran/tests/services/interfaces/ICompanyActionsService.java
@@ -1,8 +1,20 @@
 package tel_ran.tests.services.interfaces;
+
+import java.util.Date;
+import java.util.List;
+
 public interface ICompanyActionsService {
 void printQuestion(int id);
 
 //alexfoox Company 
 boolean createCompany(String C_Name,String C_Site, String C_Specialization,String C_AmountEmployes,String C_Password);
 String []getAnySingleQuery(String strQuery);
+
+//Company actions for 3.1.4. Viewing test results
+List<String> getTestsResultsAll(int currCompanyId);
+List<String> getTestsResultsForTimeInterval(int currCompanyId, Date timeFrom, Date timeUntil);
+List<String> getTestsResultsForPersonID(int currCompanyId, int PersonID);
+//getPicturesFromTest(int testID);
+
+
 }
