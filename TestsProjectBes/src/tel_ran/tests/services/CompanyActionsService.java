2f1bb39641a44e1099fc04efde9e149e19736908
Igor <igor.tymoshchuk@gmail.com>
2015-02-08 05:18:56 +0200

    Filled with templates for interface implementation

--- TestsProjectBes/src/tel_ran/tests/services/CompanyActionsService.java
+++ TestsProjectBes/src/tel_ran/tests/services/CompanyActionsService.java
@@ -1,5 +1,8 @@
 package tel_ran.tests.services;
 
+import java.util.Date;
+import java.util.List;
+
 import tel_ran.tests.services.interfaces.ICompanyActionsService;
 
 public class CompanyActionsService extends TestsPersistence implements ICompanyActionsService {
@@ -21,6 +24,25 @@ public class CompanyActionsService extends TestsPersistence implements ICompanyA
 	public void printQuestion(int arg0) {
 		// TODO Auto-generated method stub
 		
+	}
+// 	
+	@Override
+	public List<String> getTestsResultsAll(int companyID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> getTestsResultsForPersonID(int companyID, int personID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> getTestsResultsForTimeInterval(int companyID,
+			Date date_from, Date date_until) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 
