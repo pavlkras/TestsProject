78c0f703fcd1def93d5db699f2bd2f91a7b62307
israodin <israodin@gmail.com>
2015-02-11 23:13:32 +0200

    Create Company  Two  with  id

---
+++ TestsProjectBes/src/tel_ran/tests/services/CompanyTwo.java
@@ -0,0 +1,59 @@
+package tel_ran.tests.services;
+import javax.persistence.*;
+
+import java.util.*;
+@Entity
+public class CompanyTwo {
+	@Id
+	private String C_Name;
+	private String C_Site;
+		
+	private String C_Specialization;
+	
+	private String C_AmountEmployes;
+	
+	private String C_Password;
+
+	public void setPassword(String password) {
+		C_Password = password;
+	}
+	public String getPassword() {
+		return C_Password;
+	}
+	public String getC_Name() {
+		return C_Name;
+	}
+	public void setC_Name(String c_Name) {
+		C_Name = c_Name;
+	}
+	public String getC_Site() {
+		return C_Site;
+	}
+	public void setC_Site(String c_Site) {
+		C_Site = c_Site;
+	}
+	public String getC_Specialization() {
+		return C_Specialization;
+	}
+	public void setC_Specialization(String c_Specialization) {
+		C_Specialization = c_Specialization;
+	}
+	public String getC_AmountEmployes() {
+		return C_AmountEmployes;
+	}
+	public void setC_AmountEmployes(String c_AmountEmployes) {
+		C_AmountEmployes = c_AmountEmployes;
+	}
+
+	@Override
+	public String toString() {
+		return "Company " + C_Name + ", Site : " + C_Site
+				+ ", Specialization : " + C_Specialization
+				+ ", Amount Employes : " + C_AmountEmployes 
+				;
+	}
+
+
+}
+
+
