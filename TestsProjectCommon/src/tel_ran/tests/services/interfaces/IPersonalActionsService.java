8ed546630dd517ad3deb9548995f3eab5c6fd7ab
Yulatax <yulatax@gmail.com>
2015-02-13 02:58:51 +0200

    Updating interfaces of Personal part.
    Adding interface methods to IPersonalService. Updating interfaces.jar.

--- TestsProjectCommon/src/tel_ran/tests/services/interfaces/IPersonalActionsService.java
+++ TestsProjectCommon/src/tel_ran/tests/services/interfaces/IPersonalActionsService.java
@@ -1,5 +1,16 @@
 package tel_ran.tests.services.interfaces;
 
+import java.util.List;
+
 public interface IPersonalActionsService {
 
+	 List<String> getCategoriesList();
+	 String getMaxCategoryQuestions(String catName);
+	 int testCreationService(String userId);
+	 boolean saveUserService(String[] args);
+	 String[] loadUserservice(String userId);
+	 int testCreationByCategory(String userId, String category, int level, int qAmount);
+	 String[] loadTestService(int id);
+	 List<Integer> getLevelsList();
+	 String loadXMLTest(int id);
 }
