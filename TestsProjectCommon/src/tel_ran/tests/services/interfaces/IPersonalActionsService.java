dee40f70297586fbc7f36f1ecf9ab14ba83df6dd
Max Petrov <petrmax@gmail.com>
2015-02-17 23:30:42 +0200

    add methods in IPesonalActionsService

--- TestsProjectCommon/src/tel_ran/tests/services/interfaces/IPersonalActionsService.java
+++ TestsProjectCommon/src/tel_ran/tests/services/interfaces/IPersonalActionsService.java
@@ -3,8 +3,13 @@ package tel_ran.tests.services.interfaces;
 import java.util.List;
 
 public interface IPersonalActionsService {
-
-	 List<String> getCategoriesList();
-	 List<String> getComplexityLevelList();
-	 String getMaxCategoryLevelQuestions(String catName, String complexityLevel);
+	
+	boolean saveUserService(String[] args);
+	String[] loadUserservice(String userId);
+	List<String> getCategoriesList();
+	String getMaxCategoryQuestions(String catName, String level);
+	String loadXMLTest(int id);
+	String getTraineeQuestions(String category, int level, int qAmount);
+	List<String> getComplexityLevelList();
+	String getMaxCategoryLevelQuestions(String catName, String complexityLevel);
 }
