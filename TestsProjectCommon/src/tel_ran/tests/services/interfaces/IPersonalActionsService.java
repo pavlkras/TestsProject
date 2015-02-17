a485beafc2d3db6559a696b685394c336c10bc7f
Yulatax <yulatax@gmail.com>
2015-02-17 03:18:00 +0200

    Updating interface for trainee mode

--- TestsProjectCommon/src/tel_ran/tests/services/interfaces/IPersonalActionsService.java
+++ TestsProjectCommon/src/tel_ran/tests/services/interfaces/IPersonalActionsService.java
@@ -5,12 +5,6 @@ import java.util.List;
 public interface IPersonalActionsService {
 
 	 List<String> getCategoriesList();
-	 String getMaxCategoryQuestions(String catName);
-	 int testCreationService(String userId);
-	 boolean saveUserService(String[] args);
-	 String[] loadUserservice(String userId);
-	 int testCreationByCategory(String userId, String category, int level, int qAmount);
-	 String[] loadTestService(int id);
-	 List<Integer> getLevelsList();
-	 String loadXMLTest(int id);
+	 List<String> getComplexityLevelList();
+	 String getMaxCategoryLevelQuestions(String catName, String complexityLevel);
 }
