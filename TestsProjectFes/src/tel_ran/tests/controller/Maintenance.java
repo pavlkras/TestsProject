baf170c03b6992a2b70b4964b76361cef82fb47d
Max Petrov <petrmax@gmail.com>
2015-02-19 18:01:19 +0200

    fixes

--- TestsProjectFes/src/tel_ran/tests/controller/Maintenance.java
+++ TestsProjectFes/src/tel_ran/tests/controller/Maintenance.java
@@ -1,7 +1,9 @@
 package tel_ran.tests.controller;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
