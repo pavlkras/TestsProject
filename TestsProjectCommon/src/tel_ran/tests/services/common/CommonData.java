b916c4b616c475c067b8ddb8e3f2a2cb98f1e79c
lera-v <vvolynkina@rambler.ru>
2015-03-12 15:18:25 +0200

    added data for viewing tests results

--- TestsProjectCommon/src/tel_ran/tests/services/common/CommonData.java
+++ TestsProjectCommon/src/tel_ran/tests/services/common/CommonData.java
@@ -2,4 +2,7 @@ package tel_ran.tests.services.common;
 
 public class CommonData {
 	public static final String delimiter = "/--/";
+	public static final String TESTS_RESULTS = "/all_tests_results";
+	public static final String TESTS_RESULTS_BY_DATES = "/tests_results_by_dates";
+	public static final String TESTS_RESULTS_BY_PERSON_ID = "/tests_results_by_person";
 }
