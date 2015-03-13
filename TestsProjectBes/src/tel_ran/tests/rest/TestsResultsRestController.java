5d35fd7e234efd11e1df8eb738ce0fa93c5da6e2
lera-v <vvolynkina@rambler.ru>
2015-03-13 22:33:15 +0200

    added method to Rest service

--- TestsProjectBes/src/tel_ran/tests/rest/TestsResultsRestController.java
+++ TestsProjectBes/src/tel_ran/tests/rest/TestsResultsRestController.java
@@ -1,5 +1,8 @@
 package tel_ran.tests.rest;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,4 +40,17 @@ public class TestsResultsRestController {
 		return res;
 	}
 	
+	@RequestMapping(value=ICommonData.TESTS_RESULTS_BY_DATES + "/{companyName}" + "/{date1}" + "/{date2}", method=RequestMethod.GET)
+	//if format of Date dd-MM-yyyy 
+	@ResponseBody List<String> getTestsResultsByDates(@PathVariable String companyName, @PathVariable String date1, @PathVariable String date2){   
+		List<String> res = null;
+		System.out.println("date1: " + date1 + " date2: " + date2);
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+		try {
+			Date date_from = dateFormat.parse(date1);
+			Date date_until = dateFormat.parse(date2);
+			res = company.getTestsResultsForTimeInterval(companyName, date_from, date_until);
+		} catch (ParseException e) {}
+		return res;
+	}
 }
