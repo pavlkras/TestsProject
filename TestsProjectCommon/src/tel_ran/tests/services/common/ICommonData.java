d629aa5bf8e606b4ea70681b38948244ba0f1a84
lera-v <vvolynkina@rambler.ru>
2015-03-13 14:10:09 +0200

    Revert "usfhksdjvhk"
    This reverts commit 54d2f8f01faef88b55c61f0d748370a9270c5c9c.

--- TestsProjectCommon/src/tel_ran/tests/services/common/ICommonData.java
+++ TestsProjectCommon/src/tel_ran/tests/services/common/ICommonData.java
@@ -5,4 +5,3 @@ public interface ICommonData {
 	static final String TESTS_RESULTS_BY_DATES = "/tests_results_by_dates";
 	static final String TESTS_RESULTS_BY_PERSON_ID = "/tests_results_by_person";
 }
-//
