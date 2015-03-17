7c8926871c199abb15627fc351135e8827a27ff7
fen1979 <00000001@nm.ru>
2015-03-17 20:56:31 +0200

    added new methods

--- TestsProjectCommon/src/tel_ran/tests/services/interfaces/IMaintenanceService.java
+++ TestsProjectCommon/src/tel_ran/tests/services/interfaces/IMaintenanceService.java
@@ -2,23 +2,27 @@ package tel_ran.tests.services.interfaces;
 
 import java.util.List;
 
-public interface IMaintenanceService {//общий интерфейс 
+public interface IMaintenanceService {
 	public static final String DELIMITER = "----";// delimiter for entity's and filling from file
-	//Сюда добавляем свои методы для работы с ними через веб. 
-	public void setAutorization(boolean flagAutorized);
-	boolean createQuestion(String questionText,String descriptionText,String category,int level,List<String> answers,int trueAnswerNumber);
-	//Метод обновляет вопрос в БД
-	boolean UpdateQuestionInDataBase(String questionID, String questionText,
-			String descriptionText, String category, int question_level,
-			List<String> answer, int trueAnswerNumber);
-	//Поиск по БД возвращает Лист Стрингов : принимает 2 параметра но поиск идет по question
-	public List<String> SearchQuestionInDataBase(String question, String category);
-	//Метод добавления данных в БД из файла на удаленном компьютере
-	boolean FillDataBaseFromTextResource(List<String> inputParsedText);		
-	//Возвращает 1 вопрос с ответами в стринге: поиск по ID Question
+	//
+	boolean setAutorization(String username, String password);
+	//
+	boolean ModuleForBuildingQuestions(String category,int nQuestion);// Auto Generation many questions by any parameters	
+	//
+	boolean CreateNewQuestion(String imageLink, String questionText, String category, int levelOfDifficulti, List<String> answers, char correctAnswer,int id);
+	//
+	public List<String> SearchAllQuestionInDataBase(String category, int levelOfDifficulti);
+	//
 	public String getQuestionById(String questionID);
 	//
+	public List<String> getAllCategoriesFromDataBase();
+	//
+	boolean UpdateTextQuestionInDataBase(String questionID, String imageLink, String questionText, String category, int levelOfDifficulti, List<String> answers, char correctAnswer);
+	//
 	public String deleteQuetionById(String questionID);
+	//
+	boolean FillDataBaseFromTextResource(List<String> inputParsedText);	
+
 	// method for group generate test. AlexFoox returned Long ID of question 
 	public List<Long> getUniqueSetQuestionsForTest(String category,String level,Long nQuestion);
 }
