cdc2cf915ee523da049f0a212b79374d7cd5b016
Igor Tymoshchuk <igor.tymoshchuk@gmail.com>
2015-03-28 20:43:04 +0300

    Interface changed
    companyId changes

--- TestsProjectCommon/src/tel_ran/tests/services/interfaces/ICompanyActionsService.java
+++ TestsProjectCommon/src/tel_ran/tests/services/interfaces/ICompanyActionsService.java
@@ -19,8 +19,8 @@ long createIdTest(List<Long> list,int personId, String testPassword);
 int createPerson(int personId,String personName,String personSurname,String personEmail);
 
 //Company actions for 3.1.4. Viewing test results
-String getTestsResultsAll(String currCompanyId);
-String getTestsResultsForTimeInterval(String currCompanyId, Date timeFrom, Date timeUntil);
-String getTestsResultsForPersonID(String currCompanyId, int personID);
-
+public String getTestsResultsAll(long companyId);
+public String getTestsResultsForPersonID(long companyId, int personID);
+public String getTestsResultsForTimeInterval(long companyId, Date date_from, Date date_until);
+public String getTestResultDetails(long companyId, int testId);
 }
