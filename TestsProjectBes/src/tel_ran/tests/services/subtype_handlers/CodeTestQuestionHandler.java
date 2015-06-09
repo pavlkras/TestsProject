29b7f818e2fe08f9827ffac8e90e25fbbacf142d
Lev <lev.magazinnik@gmail.com>
2015-06-09 10:56:03 +0300

    Added Gradle module from: https://github.com/IgorTymoshchuk/GradleTestsSubProj.git || commit: 0cd98a0 Lev at 09/06/2015 09:51(fixed BUG)

--- TestsProjectBes/src/tel_ran/tests/services/subtype_handlers/CodeTestQuestionHandler.java
+++ TestsProjectBes/src/tel_ran/tests/services/subtype_handlers/CodeTestQuestionHandler.java
@@ -1,6 +1,9 @@
 package tel_ran.tests.services.subtype_handlers;
 
 import org.json.JSONObject;
+import tel_ran.tests.gradle_tester.CodeTester;
+
+import java.io.IOException;
 
 public class CodeTestQuestionHandler extends AbstractTestQuestionHandler implements ITestQuestionHandler{
 
@@ -11,22 +14,40 @@ public class CodeTestQuestionHandler extends AbstractTestQuestionHandler impleme
 	@Override
 	public void analyze() {
 		// TODO Gradle call
-		getQuestionAttribubes().getFileLocationLink(); //Path to zip
-		fileManager.getPathToCode(companyId, testId, getQuestionID()); //Path to person code
+//		Source code is here:
+//		https://github.com/IgorTymoshchuk/GradleTestsSubProj.git
+		String pathToAnswersZip;
+		String codeFromPersonPath;
+		CodeTester gradleModule;
+		boolean gradleAnswer;
+
+		pathToAnswersZip=getQuestionAttribubes().getFileLocationLink(); //Path to zip
+		codeFromPersonPath=fileManager.getPathToCode(companyId, testId, getQuestionID()); //Path to person code
+		try {
+
+			gradleModule = new CodeTester();
+//----------NEED TO PROCEED SOMEWHERE WITH GRADLE ANSWER
+			gradleAnswer=gradleModule.testIt(codeFromPersonPath,pathToAnswersZip);
+//--------------------------------
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
+
 
 	@Override
 	public boolean setPersonAnswer(JSONObject answerJsonObj) {
 		// TODO Auto-generated method stub
 		// Saving fields to files
-		
+
 		return false;
 	}
 
 	@Override
 	public String getQuestionJson(int index) {
-		
-		
+
+
 		getQuestionAttribubes().getLineCod(); //stub
 		getQuestionAttribubes().getQuestionId().getQuestionText(); //question text
 		getQuestionAttribubes().getQuestionId().getDescription(); //description
