2f4e62a44c43793cc99dd590c1bc97560516d9e5
Valeriya <vvolynkina@rambler.ru>
2015-07-16 23:56:16 +0300

    Test page for Person

--- TestsProjectFes/WebContent/WEB-INF/js_folder/person_scripts/testController.js
+++ TestsProjectFes/WebContent/WEB-INF/js_folder/person_scripts/testController.js
@@ -1,13 +1,6 @@
 /**
- *  Test control mode angular controller
+ *  Test control mode test controller
  */
-
-/*var app = angular.module('app', ['smart-table','mgcrea.ngStrap','ngDialog']);
-
-app.controller('QuestionTestController', ['$scope','$http', 'ngDialog', function($scope, $http, ngDialog) {
-	
-	
-}]); */
 
 var app = angular.module("testPage", []);
 
@@ -17,98 +10,92 @@ app.controller("QuestionTestController",function($scope, $http) {
     $scope.mySwitchEndTest = false;
     $scope.mySwitchAmericanSystemTestQuestion = false;
     $scope.mySwitchCodeTestQuestion = false;
+    $scope.mySwitchImage = false;
 
     $scope.toggleShowDetails = function(){
         $scope.mySwitchStartTest = !$scope.mySwitchStartTest;
-        $scope.getQuestion(0);  //+ send keyTest to BES
+        $scope.getQuestion(null);  
     }
 
-    // Request ----------------------------------------------------------------
     $scope.getQuestion = function(userAnswer){
-        console.log("in getQuestion answer = " + userAnswer);
-            var link = "/TestsProjectBes/personTest/saveprev_getnext";
-          /*  $scope.httpConfig = {
-                headers: {
-                    'Authorization': $scope.token
-                }
-            };*/
+        var link = "/TestsProjectBes/persontest/saveprev_getnext";
+        $scope.httpConfig = {
+             headers: {
+                  'Authorization': $scope.token
+             }
+        };
         var dataObj = {
             answer: userAnswer
             //index: nQuestion  //?
         };
         
-            //$http.post(link, dataObj, $scope.httpConfig).success(function (response) {
-        $http.post(link, dataObj).success(function(data, status, headers, config) {
+        $http.post(link, dataObj, $scope.httpConfig).success(function (data, status, headers, config) {
+            	console.log("Success - request result to Rest");
                 $scope.question = data;
                 if ($scope.question == null) {
+                	console.log("data from rest (question) is null");
                     $scope.mySwitchEndTest = !$scope.mySwitchEndTest;
                     $scope.mySwitchShowTest = !$scope.mySwitchShowTest;
                 }
                 else {
                     $scope.mySwitchShowTest = true;
-                    //$scope.question = response;
-                    $scope.numberQuestion = $scope.question[0].index;
-                    $scope.text = $scope.question[0].question_text;
-                    $scope.image = $scope.question[0].question_image;            //if > 30 ->question with image (not codeAnswer)
-                                                                                 //decoding!
-                    $scope.numberAnswers = $scope.question[0].n_answers;
+                    $scope.numberQuestion = $scope.question.index;
+                    $scope.text = $scope.question.question_text;
+                    $scope.image = $scope.question.question_image; 
+                    if($scope.image != null){
+                    	$scope.mySwitchImage = true;
+                    }
+                    else{
+                    	$scope.mySwitchImage = false;
+                    }
+                    $scope.type_question = $scope.question.typeQuestion;                                                    
+                 
+                    if($scope.type_question == 1) {                                  //Type of question - AmericanSystemQuestion
+                        $scope.numberAnswers = $scope.question.n_answers;
+                        $scope.nAnswers = $scope.getAnswers($scope.numberAnswers);
+                        $scope.mySwitchAmericanSystemTestQuestion = true;
+                        $scope.mySwitchCodeTestQuestion = false;
+                    }
+                    if($scope.type_question == 2){                                   //Type of question - CodeQuestion
+                        $scope.mySwitchAmericanSystemTestQuestion = false;
+                        $scope.mySwitchCodeTestQuestion = true;
+                    }
+                    if($scope.type_question == 0){                                   //End Test
+                        $scope.mySwitchShowTest = false;
+                        $scope.mySwitchEndTest = true;
+                    }
+                    
                     data = null;
+                    userAnswer = null;
+                    $scope.answer = {
+                        name: null
+                    }
                 }
+                
         }).error(function (data, status, headers, config){
-            $scope.mySwitchShowTest = true;
-
-            $http.get("question1.txt").success(function(response){
-                $scope.question = response;
-                $scope.numberQuestion = $scope.question[0].index;
-                $scope.text = $scope.question[0].question_text;
-                $scope.image = $scope.question[0].question_image;
-                $scope.type_question = $scope.question[0].typeQuestion;
-                //console.log("$scope.typeQuestion = " + $scope.type_question);
-                if($scope.type_question == 1) {                       //AmericanSystemQuestion
-                    $scope.numberAnswers = response[0].n_answers;
-                    $scope.nAnswers = [];
-                    $scope.addToArray();
-                    $scope.mySwitchAmericanSystemTestQuestion = true;
-                    $scope.mySwitchCodeTestQuestion = false;
-                }
-                if($scope.type_question == 2){                        //CodeQuestion
-                    //hidden template for AmericanSystemQuestion
-                    $scope.mySwitchAmericanSystemTestQuestion = false;
-                    $scope.mySwitchCodeTestQuestion = true;
-                }
-                console.log("$scope.numberAnswers " + $scope.numberAnswers);
-                response = null;
-                userAnswer = null;
-                $scope.answer = {
-                    name: null
-                }
-
-
-            });
-            //console.log("in cntrl" + $scope);
-            //console.log("in Error response e: " + $scope.numberAnswers);
+        	console.log("Error - request result to Rest");       
         });
     };
 
-    $scope.addToArray = function() {
-        for(i=0; i<$scope.numberAnswers; i++){
-            $scope.nAnswers.push(i + 1);
-            //console.log("array " + $scope.nAnswers[i]);
+
+    $scope.getAnswers = function(number) {         //filling array of answers
+    	array = [];
+        for(i=0; i<number; i++){
+        	array.push(i + 1);
         }
+        return array;
     };
+    
 });
 
 app.directive('testPage', function() {
     return {
-
         replace: false,
         scope: false,
 
-        templateUrl: /TestsProjectFes/static/js_folder/person_scripts/testQuestionPage.html,
+        templateUrl: "/TestsProjectFes/static/js_folder/person_scripts/testQuestionPage.html",
 
-        link: function foo(scope, element, attrs){
-            console.log("in directive " + scope.nAnswers);
-        }
+        link: function foo(scope, element, attrs){}
     }
 });
 
