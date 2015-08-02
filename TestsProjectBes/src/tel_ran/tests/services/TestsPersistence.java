c4dc919bf05cb1b89bb405d591e9abe8b90b8213
Maria Go <margilit@gmail.com>
2015-08-02 15:37:09 +0300

    minor changes
    only a few spaces for readability

--- TestsProjectBes/src/tel_ran/tests/services/TestsPersistence.java
+++ TestsProjectBes/src/tel_ran/tests/services/TestsPersistence.java
@@ -1,9 +1,16 @@
 package tel_ran.tests.services;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceContextType;
-public class TestsPersistence {
-@PersistenceContext(unitName="springHibernate",
-type=PersistenceContextType.TRANSACTION)
-public EntityManager em;
+
+public abstract class TestsPersistence {
+	
+	@PersistenceContext(unitName="springHibernate",
+	type=PersistenceContextType.TRANSACTION)
+	
+	public EntityManager em;
+	
+
+		
 }
