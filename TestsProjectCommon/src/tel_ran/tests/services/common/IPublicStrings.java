ed5c5e92cb4565b9ac72252da996ce43a7317fe2
Maria Go <margilit@gmail.com>
2015-08-04 14:31:09 +0300

    minor changes

--- TestsProjectCommon/src/tel_ran/tests/services/common/IPublicStrings.java
+++ TestsProjectCommon/src/tel_ran/tests/services/common/IPublicStrings.java
@@ -9,8 +9,8 @@ public interface IPublicStrings {
 	static final String COMPANY_QUESTION_QUESTION = "Type your answer in the textbox bellow";
 	
 	static final String[] CREATE_TEST_ERROR = {"The test was successfully created and sended by e-mail", // - 0
-		"Not enough questions in the database to generate the test./n " 
-		+ "Please fill in the database or create one another test with fewer questions", // - 1
+		"Not enough questions in the database to generate the test." 
+		+ "Please fill in the database or create one another test with fewer number of questions", // - 1
 		"Invalid value in the E-mail box",  // - 2 
 		"Not enough data to generate the text./n For category * should be specified sub-category", // - 3
 		"The test was not created. There're some problem. Please contact your support", // - 4
