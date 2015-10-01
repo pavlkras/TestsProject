883e85bcf0027b7ac7a57fec6a83ff6603fa2d04
Maria Go <margilit@gmail.com>
2015-10-01 13:46:02 +0300

    test process changed
    Now we've returned to the REST - 1 answer sending - 1 question getting

--- TestsProjectCommon/src/tel_ran/tests/services/interfaces/IPersonalActionsService.java
+++ TestsProjectCommon/src/tel_ran/tests/services/interfaces/IPersonalActionsService.java
@@ -1,11 +1,19 @@
 package tel_ran.tests.services.interfaces;
 
 public interface IPersonalActionsService extends ICommonService{
-	public boolean GetTestForPerson(String pssswordForCreatedTest);	
-	public String getNextQuestion(long testId);
-	public void setAnswer(long testId, String jsonAnswer);
-	public String getToken(String password);
-	public boolean testIsPassed(long testId);
-	void saveImage(long testId, String image);
-	public String getAllTestQuestions(long testId);
+	
+	boolean GetTestForPerson(String pssswordForCreatedTest);	
+	String getToken(String password);
+	void saveImage(long testId, String image);		
+
+	
+	/**
+	 * method for TEST
+	 * save an answer of person and return the next test question if it exists
+	 * If questions have been finished it start the process of results checking
+	 * @param testId = long
+	 * @param answer = JSON
+	 * @return JSON string with question
+	 */
+	String saveAndGetNextQuestion(long testId, String answer);
 }
