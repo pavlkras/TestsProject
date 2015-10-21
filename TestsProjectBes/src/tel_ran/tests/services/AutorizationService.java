bad90a3ea98442cfb51a09e5b9f447e0ffb16c6e
Maria Go <margilit@gmail.com>
2015-10-21 22:18:15 +0300

    Renewing structure - 1: new Rest controller
    Added new REST-controller for actions that don't need autorization and
    for registration and login. Added new Service for these processes. Added
    class User that contains info about ID and role (user, company...)

---
+++ TestsProjectBes/src/tel_ran/tests/services/AutorizationService.java
@@ -0,0 +1,223 @@
+package tel_ran.tests.services;
+
+import json_models.AutorizationModel;
+import json_models.ErrorModel;
+import json_models.RegistrationModel;
+
+import org.json.JSONException;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import tel_ran.tests.data_loader.IDataLoader;
+import tel_ran.tests.services.common.ICommonData;
+import tel_ran.tests.services.fields.Role;
+import tel_ran.tests.services.utils.SpringApplicationContext;
+import tel_ran.tests.token_cipher.TokenProcessor;
+
+public class AutorizationService {
+
+	private IDataLoader autoData;	
+	private static AutorizationService service;		
+	private TokenProcessor tokenProcessor;
+	private static final int TOKEN_VALID_IN_SECOND = 86400; // 24h 
+	
+	private AutorizationService() {	
+		autoData = (IDataLoader) SpringApplicationContext.getBean("autoData");
+		tokenProcessor = (TokenProcessor) SpringApplicationContext.getBean("tokenProc");
+	}
+	
+	public static AutorizationService getInstance() {
+		if(service==null) {
+			service = new AutorizationService();
+		}
+		return service;
+	}
+	
+	public String testLogIn(String loginJson) {		
+		
+		//1 - get Data from JSON
+		AutorizationModel model = null;
+		try {
+			model = new AutorizationModel(loginJson);
+		} catch (JSONException e) {			
+			e.printStackTrace();
+			try {
+				return ErrorModel.getJson("Incorrect JSON");
+			} catch (JSONException e1) {				
+				e1.printStackTrace();			
+			}
+		}
+		
+		//2 - check if it's user data
+		long id = autoData.checkUserLogIn(model.getLogin(), model.getPassword());
+		
+		
+		//3 - get token and info 
+		if(id>=0) {
+			boolean admin = autoData.isAdmin(id);
+			if(admin) {				
+				model.setAdmin(true);
+				model.setRoleNumber(Role.ADMINISTRATOR.ordinal());
+			} else {
+				model.setRoleNumber(Role.USER.ordinal());
+			}
+			model.setToken(this.encodeToken(id, model.getRoleNumber()));	
+			autoData.fillInfoAboutUser(model, id);
+		}
+		
+		
+		//4 - preparing result
+		String result = null;		
+		try {
+			result =  model.getString();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	
+	public String testSignIn(String request) {
+		
+		
+		//1 - get Data from JSON
+		RegistrationModel model = null;
+		try {
+			model = new RegistrationModel(request);
+		} catch (JSONException e) {			
+			e.printStackTrace();
+			try {
+				return ErrorModel.getJson("Incorrect JSON");
+			} catch (JSONException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+		
+		//2 - check if this email is in DB
+		boolean emailIsInDb = autoData.checkUserEmail(model.getLogin());
+		model.setUserExist(emailIsInDb);
+		
+		//3 - registration
+		if(!emailIsInDb) {
+			//3a - check id Admin is created
+			boolean isAdmin = autoData.checkRootUser();
+			if(!isAdmin) {
+				adminCreation();
+			}
+			
+			//3b - registration
+			boolean registrated = autoData.userRegistration(model.getLogin(), model.getPassword(),
+					model.getFirstName(), model.getLastname(), model.getNickname());
+			
+			model.setSuccess(registrated);
+		}		
+		//4 preparing answer
+		
+		String result = null;
+		try {
+			result = model.getString();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
+	private void adminCreation() {
+		String email = "admin@com";
+		String password = "12345.com";
+		boolean isAdmin = true;
+		autoData.userRegistration(email, password, isAdmin);
+		
+	}
+	
+	private String encodeToken(long id, int role) {
+		String token = tokenProcessor.encodeRoleToken(id, role, TOKEN_VALID_IN_SECOND);
+		return token;
+	}
+	
+	public String companyLogIn(String request) {
+		
+		//1 - get Data from JSON
+		AutorizationModel model = null;
+		try {
+			model = new AutorizationModel(request);
+		} catch (JSONException e) {			
+			e.printStackTrace();
+			try {
+				return ErrorModel.getJson("Incorrect JSON");
+			} catch (JSONException e1) {				
+				e1.printStackTrace();			
+			}
+		}
+			
+		//2 - check if it's existing data	
+		long id = autoData.checkCompanyLogIn(model.getLogin(), model.getPassword());
+		
+		//3 - get token and info 
+		if(id>=0) {
+			model.setRoleNumber(Role.COMPANY.ordinal());			
+			model.setToken(this.encodeToken(id, model.getRoleNumber()));	
+			autoData.fillInfoAboutCompany(model, id);
+		}		
+		
+		//4 - preparing result
+		String result = null;		
+		try {
+			result =  model.getString();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	public String companySignUp(String request) {
+		
+		//1 - get Data from JSON
+		RegistrationModel model = null;
+		try {
+			model = new RegistrationModel(request);
+		} catch (JSONException e) {			
+			e.printStackTrace();
+			try {
+				return ErrorModel.getJson("Incorrect JSON");
+			} catch (JSONException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+		
+		//2 - check if this login is in DB
+		boolean companyExists = autoData.checkCompanyName(model.getLogin());
+		model.setUserExist(companyExists);
+		
+		//3 - registration
+		if(!companyExists) {
+			boolean registrated = autoData.companyRegistration(model.getLogin(), model.getPassword(),
+					model.getEmployesNumber(), model.getWebSite(), model.getSpec());
+			
+			model.setSuccess(registrated);
+		}		
+		//4 preparing answer
+		
+		String result = null;
+		try {
+			result = model.getString();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return result;
+
+	}
+
+	public boolean findCompany(String name_company) {
+		boolean result = autoData.checkCompanyName(name_company);
+		
+		return result;
+	}
+}
--- TestsProjectBes/src/tel_ran/tests/token_cipher/TokenProcessor.java
+++ TestsProjectBes/src/tel_ran/tests/token_cipher/TokenProcessor.java
@@ -1,24 +1,23 @@
 package tel_ran.tests.token_cipher;
 
 import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import tel_ran.tests.services.common.ICommonData;
 
 @Component
 public class TokenProcessor {
 	
 	@Autowired
 	AESCipher cipher;
+	private static final String DELIMETER = ",";
 	
 	public String encodeIntoToken(long id, int validTimeInSec) {
 		long currTimeStamp = new Date().getTime();
 		long validUntilTimeStamp = currTimeStamp + validTimeInSec * 1000;
 		StringBuffer strbuf = new StringBuffer();
 		strbuf.append(Long.toString(validUntilTimeStamp)); // array index #0
-		strbuf.append(ICommonData.delimiter);
+		strbuf.append(DELIMETER);
 		strbuf.append(id); // array index #1
 		String token = "";
 		try {
@@ -36,7 +35,7 @@ public class TokenProcessor {
 			wasDecrypted = cipher.decrypt(token);
 			//System.out.println("wasDecrypted: "+wasDecrypted);
 			
-			String [] array = wasDecrypted.split(ICommonData.delimiter);
+			String [] array = wasDecrypted.split(DELIMETER);
 			long currTimeStamp = new Date().getTime();
 			long validUntilTimeStamp = Long.parseLong(array[0]);
 			if(validUntilTimeStamp > currTimeStamp){
@@ -44,6 +43,45 @@ public class TokenProcessor {
 			}
 		} catch (Exception e) {}
 		return companyId;
+	}
+	
+	public String encodeRoleToken(long id, int role, int validTimeInSec) {
+		long currentTimeStamp = System.currentTimeMillis();
+		long validUntilTime = currentTimeStamp + validTimeInSec * 1000;
+		StringBuilder strb = new StringBuilder();
+		strb.append(validUntilTime); //index 0
+		strb.append(DELIMETER);
+		strb.append(role);//index 1
+		strb.append(DELIMETER);
+		strb.append(id);//index 2
+		String token = "";
+		try {
+			token = cipher.encrypt(strb.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return token;
+	}
+	
+	public User decodeRoleToken(String token) {
+		int role = -1; //ERRORSTATE
+		long id = -1; // ERRORSTATE = -1;
+		String wasDecrypted = "";
+		
+		try {
+			wasDecrypted = cipher.decrypt(token);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		String[] array = wasDecrypted.split(DELIMETER);
+		long validUntilTime = Long.parseLong(array[0]);
+		long currentTime = System.currentTimeMillis();
+		if(validUntilTime > currentTime) {
+			role = Integer.parseInt(array[1]);
+			id = Long.parseLong(array[2]);
+		}
+	
+		return new User(id, role);	
 	}
 
 }
