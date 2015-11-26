7895517a216876236ffff89e862f2a6f41875d32
Maria Go <margilit@gmail.com>
2015-11-26 23:45:50 +0200

    renamings

---
+++ TestsProjectBes/src/json_models/ResultAndErrorModel.java
@@ -0,0 +1,48 @@
+package json_models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import tel_ran.tests.services.common.IPublicStrings;
+
+public class ResultAndErrorModel {
+	
+	
+	public static String getJson(String str) throws JSONException {
+		
+		JSONObject obj = new JSONObject();
+		obj.put(JSONKeys.ERROR, str);
+		return obj.toString();
+	}
+	
+	public static String getJson(String str, int errorCode) throws JSONException {
+		
+		JSONObject obj = new JSONObject();
+		obj.put(JSONKeys.ERROR, str);
+		obj.put(JSONKeys.ERROR_CODE, errorCode);
+		return obj.toString();
+		
+	}
+	
+	public static String getJson(int errorcode) throws JSONException {
+		JSONObject obj = new JSONObject();
+		obj.put(JSONKeys.ERROR, IPublicStrings.CREATE_TEST_ERROR[errorcode]);
+		obj.put(JSONKeys.ERROR_CODE, errorcode);
+		return obj.toString();
+	}
+	
+	public static String getAnswer(String answer, int code) throws JSONException {
+		JSONObject jsn = new JSONObject();
+		jsn.put(JSONKeys.RESPONSE_TEXT, answer);
+		jsn.put(JSONKeys.RESPONSE_CODE, code);
+		return jsn.toString();
+	}
+	
+	public static String getResponse(String key, Object value, int code) throws JSONException {
+		JSONObject jsn = new JSONObject();
+		jsn.put(key, value);
+		jsn.put(JSONKeys.RESPONSE_CODE, code);
+		return jsn.toString();
+	}
+	
+
+}
