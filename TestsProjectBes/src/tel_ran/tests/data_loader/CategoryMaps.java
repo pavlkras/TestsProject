051815521cbb5430c51d9b3fb0b553e73d7eea04
Maria Go <margilit@gmail.com>
2015-11-26 23:49:30 +0200

    TestService

--- TestsProjectBes/src/tel_ran/tests/data_loader/CategoryMaps.java
+++ TestsProjectBes/src/tel_ran/tests/data_loader/CategoryMaps.java
@@ -5,67 +5,37 @@ import java.util.List;
 import java.util.Map;
 
 import json_models.CategoriesList;
+import json_models.IJsonModels;
 
+import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import tel_ran.tests.processor.TestProcessor;
 import tel_ran.tests.services.fields.Role;
 import tel_ran.tests.services.utils.SpringApplicationContext;
 
 public final class CategoryMaps {
-	
-	private static CategoryMaps instance;
-	private IDataTestsQuestions persistence;
-	
-	private Map<String, List<String>> AUTO_CATEGORIES; 
-	private String AUTO_CATEGORIES_JSON;
-	private Map<String, Map<String, List<String>>> ADMIN_CATEGORIES;
-	private String ADMIN_CATEGORIES_JSON;
-	
-	private CategoryMaps() {
-		renewAutoCategories();
-		persistence = (IDataTestsQuestions) SpringApplicationContext.getBean("testQuestsionsData");
-		renewAdminCategories();
-	}	
-	
-	public static CategoryMaps getInstance() {
-		if(instance==null) {
-			synchronized (CategoryMaps.class) {
-				if(instance==null)
-					instance = new CategoryMaps();
-			}			
-		}		
-		return instance;		
-	}
-	
-	public String getJsonAdminCategories() {
-		return this.ADMIN_CATEGORIES_JSON;
-	}
-	
-	public String getJsonAutoCategories() {
-		return this.AUTO_CATEGORIES_JSON;
-	}
-	
-	private void renewAdminCategories() {
-		List<String> categories1 = persistence.getUserCategories(-1, Role.ADMINISTRATOR);
-		this.ADMIN_CATEGORIES = new HashMap<String, Map<String, List<String>>>();
-		if(categories1!=null) {
-			for(String s : categories1) {				
-				Map<String, List<String>> categories2 = persistence.getCustomCategories2WithMetaCategory(s, -1, Role.ADMINISTRATOR);
-				this.ADMIN_CATEGORIES.put(s, categories2);
-			}			
-		}
-		try {
-			ADMIN_CATEGORIES_JSON = getCategoriesWithMCJson(this.ADMIN_CATEGORIES);
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
 		
+	private static Map<String, List<String>> AUTO_CATEGORIES; 
+	private static String AUTO_CATEGORIES_JSON;	
+	
+	
+	static {
+		renewAutoCategories();		
 	}
 	
-	private void renewAutoCategories() {
+	public CategoryMaps() {		
+	
+	}	
+
+	public static String getJsonAutoCategories() {
+		return AUTO_CATEGORIES_JSON;
+	}
+	
+	private static void renewAutoCategories() {
 		List<String> metaCategories = TestProcessor.getMetaCategory();
-		this.AUTO_CATEGORIES = new HashMap<String, List<String>>();
+		AUTO_CATEGORIES = new HashMap<String, List<String>>();
 		for(String s : metaCategories) {
 			List<String> categories = TestProcessor.getCategoriesList(s);
 			AUTO_CATEGORIES.put(s, categories);			
@@ -77,16 +47,11 @@ public final class CategoryMaps {
 		}
 	}
 	
-	private String getCategoriesJson(Map<String, List<String>> map) throws JSONException{
+	private static String getCategoriesJson(Map<String, List<String>> map) throws JSONException{
 		CategoriesList jsonModel = new CategoriesList();
 		jsonModel.setCategories(map);
 		return jsonModel.getString();
 	}
 	
-	private String getCategoriesWithMCJson(Map<String, Map<String, List<String>>> map) throws JSONException {
-		CategoriesList jsonModel = new CategoriesList();
-		jsonModel.setCategoriesWithData(map);
-		return jsonModel.getString();
-	}
 
 }
---
+++ TestsProjectBes/src/tel_ran/tests/services/TestService.java
@@ -0,0 +1,225 @@
+package tel_ran.tests.services;
+
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+import javax.persistence.Query;
+
+import json_models.CategoriesList;
+import json_models.JSONKeys;
+import json_models.ResultAndErrorModel;
+import json_models.PersonModel;
+import json_models.TemplateModel;
+import json_models.TestModel;
+
+import org.json.JSONException;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import tel_ran.tests.data_loader.CategoryMaps;
+import tel_ran.tests.data_loader.IDataTestsQuestions;
+import tel_ran.tests.data_loader.TestQuestionsData;
+import tel_ran.tests.entitys.EntityQuestionAttributes;
+import tel_ran.tests.entitys.EntityTestQuestions;
+import tel_ran.tests.services.common.ICommonData;
+import tel_ran.tests.services.common.IPublicStrings;
+import tel_ran.tests.services.fields.Role;
+import tel_ran.tests.token_cipher.User;
+
+public class TestService  {
+	
+	@Autowired
+	IDataTestsQuestions testQuestsionsData;	
+	
+	User user;
+	
+	public TestService() {}
+	
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	
+			
+	public String getCategories(User user) {
+		CategoriesList catList = testQuestsionsData.getCategoriesList(user.getRole(), user.getRoleNumber());
+		String result = "";
+		
+		try {
+			result = catList.getString();
+		} catch (JSONException e) {			
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+
+	/**
+	 * Return list of possible auto categories.
+	 * This list is saved in the Memory by CategoryMaps
+	 * @return
+	 */
+	public static String getAutoCategories() {		
+		return CategoryMaps.getJsonAutoCategories();
+	}
+	
+	/**
+	 * Return list of existing admin categories 
+	 * @return
+	 */
+	public String getAdminCategories() {
+		CategoriesList categoriesList = testQuestsionsData.getCategoriesList(Role.ADMINISTRATOR, -1);		
+		String result = "";
+		
+		try {
+			result = categoriesList.getString();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+
+	
+	
+	public String createTestAndPerson(String testInfo) {
+				
+		//0 - read info about save template
+		//1 - read info for tests
+		TemplateModel template = null;
+		try {
+			template = new TemplateModel(testInfo);			
+		} catch (JSONException e) {
+			e.printStackTrace();
+			try {
+				return ResultAndErrorModel.getJson(IPublicStrings.TEST_SOME_TROUBLE);
+			} catch (JSONException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+		
+		if(template!=null && template.isSaved()) 
+			saveTemplate(template);
+		
+		//2 - read Info for new Person
+		PersonModel person = null;
+		try {		
+			person = new PersonModel(testInfo);
+		} catch (JSONException e) {
+			e.printStackTrace();			
+			try {
+				return ResultAndErrorModel.getJson(IPublicStrings.TEST_NOT_CORRECT_PERSON_INFO);
+			} catch (JSONException e1) {				
+				e1.printStackTrace();
+			}			
+		}
+		
+		//3 - create new Test
+		template.createNewTest(this);
+		template.fillTest(this);
+		
+		//4 - save Test
+		long testId = saveTest(template.getTest(), person);
+		
+		String result = "";
+		try {
+			result = ResultAndErrorModel.getResponse(JSONKeys.TEST_ID, testId, IPublicStrings.TEST_SUCCESS);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
+	
+	private long saveTest(TestModel test, PersonModel person) {
+		long personId = testQuestsionsData.createPerson(person.getEntity());			
+				
+		return testQuestsionsData.createTest(test.getTest(), test.getQuestions(), personId, this.user.getRole(), this.user.getId());		
+	}
+
+	private void saveTemplate(TemplateModel template) {
+		testQuestsionsData.createTemplate(template.getEntity(), this.user.getRole(), this.user.getId());		
+	}
+
+	public List<EntityQuestionAttributes> getAllQuestionsByParams(
+			String metaCategory, String category1, String category2, int difficulty) {		
+		return this.testQuestsionsData.getQuestionListByParams(metaCategory, category1, category2, difficulty, this.user.getRole(),
+				this.user.getId());
+	}
+
+	public EntityQuestionAttributes findQuestionById(Long id) {		
+		return testQuestsionsData.findQuestionById(id);
+	}
+
+	public String sendTestByMail(String link, long testId) {
+		//1 - find test by id
+		
+		TestModel test = new TestModel(testQuestsionsData.findTestById(testId));		
+				
+		//2 - check test
+		if(!this.user.getRole().equals(Role.ADMINISTRATOR) && !(this.user.getRole().equals(Role.COMPANY) && 
+				test.getCompanyId() != this.user.getId())) {
+				try {
+					return ResultAndErrorModel.getJson(IPublicStrings.TEST_NO_RIGHT_TO_SEND);
+				} catch (JSONException e) {
+					
+					e.printStackTrace();
+					return "";
+				}
+		}			
+					
+		//3 - create Sender
+		boolean result;
+		
+		try {
+			result = test.sendMail(link);
+		} catch (JSONException e) {
+			e.printStackTrace();
+			try {
+				return ResultAndErrorModel.getJson(IPublicStrings.TEST_NOT_SENDED);
+			} catch (JSONException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+				return "";
+			}
+			
+		}
+		
+		if(!result) {
+			try {
+				return ResultAndErrorModel.getJson(IPublicStrings.TEST_NOT_SENDED);
+			} catch (JSONException e) {			
+				e.printStackTrace();
+				return "";
+			}			
+		} else {
+			try {
+				return ResultAndErrorModel.getAnswer("The mail was sended", 0);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return "";
+		}				
+		
+	}
+	
+
+	
+	
+	
+	
+	
+	
+	
+
+
+}
