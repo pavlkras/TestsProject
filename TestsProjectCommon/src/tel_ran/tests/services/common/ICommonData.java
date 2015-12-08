47ef87882cd2c995aeac3e878f0f32b3b2bfeb50
margilit <margilit@gmail.com>
2015-12-09 00:39:19 +0300

    minor changes in constants

--- TestsProjectCommon/src/tel_ran/tests/services/common/ICommonData.java
+++ TestsProjectCommon/src/tel_ran/tests/services/common/ICommonData.java
@@ -1,6 +1,11 @@
 package tel_ran.tests.services.common;
 
 public interface ICommonData {
+	
+	// ------------- LETTERS FOR ANSWER OPTIONS IN USER AMERICAN TESTS ----------------------- //
+	
+	public static final String[] LETTERS = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+	
 	static final String TESTS_RESULTS = "/all_tests_results";
 	static final String TESTS_RESULTS_BY_DATES = "/tests_results_by_dates";
 	static final String TESTS_RESULTS_BY_PERSON_ID = "/tests_results_by_person";
