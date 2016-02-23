a0af8cf8e735cfb6a6e6fee434999673ff13cf71
Pavlo K <pavel.krasnoshchek@gmail.com>
2016-02-23 14:13:25 +0200

    Added TestsProjectBes

--- src/tel_ran/tests/dao/IData.java
+++
@@ -1,9 +0,0 @@
-package tel_ran.tests.dao;
-
-import tel_ran.tests.entitys.Company;
-import tel_ran.tests.services.fields.Role;
-
-public interface IData {
-	
-	Company getCompanyById(int id, Role role);
-}
--- src/tel_ran/tests/services/processes/ITestProcess.java
+++
@@ -1,9 +0,0 @@
-package tel_ran.tests.services.processes;
-
-import tel_ran.tests.entitys.Test;
-
-public interface ITestProcess extends Runnable {
-	
-	void setTest(Test test);
-
-}
